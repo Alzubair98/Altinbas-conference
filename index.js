fa1000ee89c7897cabc544f5e632b719dcaafba7
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-06-21 21:19:37 +0300

    fixing the menu

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const navMenu = document.querySelector(".hamburger-button");
 const hiddenMenu = document.querySelector(".hidden-menu");
 const xbutton = document.querySelector(".menu-hamburger-button");
+const fullBody = document.body;
 
 
 navMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active")
     hiddenMenu.classList.toggle("active")
+    fullBody.classList.toggle("active")
 })
 
 xbutton.addEventListener("click", ()=>  {
     navMenu.classList.toggle("active")
     hiddenMenu.classList.toggle("active")
+    fullBody.classList.toggle("active")
 })
