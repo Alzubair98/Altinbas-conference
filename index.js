144e4f53e481f6f0905935c75a65e57fb1f90734
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-06-21 20:57:03 +0300

    add the toggler menu

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const navMenu = document.querySelector(".hamburger-button");
+const hiddenMenu = document.querySelector(".hidden-menu");
+const xbutton = document.querySelector(".menu-hamburger-button");
+
 
 navMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active")
+    hiddenMenu.classList.toggle("active")
+})
+
+xbutton.addEventListener("click", ()=>  {
+    navMenu.classList.toggle("active")
+    hiddenMenu.classList.toggle("active")
 })
