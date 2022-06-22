7c5f1172ae0d2038c56a55de62e17e41f885f17d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-06-22 21:57:54 +0300

    make the feature cards daynamic

--- index.js
+++ index.js
@@ -15,3 +15,94 @@ xbutton.addEventListener("click", ()=>  {
     hiddenMenu.classList.toggle("active")
     fullBody.classList.toggle("active")
 })
+
+
+
+const featureCards = `
+<div class="first-2-cards">
+                    <div class="first-card">
+                        <div class="images">
+                            <img class="square-img" src="images/squers.jpg" alt="backgroundimg">
+                            <img class="presenter-img" src="images/oguz-ata.jpg" alt="presenter-img">
+                        </div>
+                        <div class="card-text">
+                            <h2 class="presenter-name">Oğuz ata</h2>
+                            <p class="presenter-descreption">Head of IT department at altinbas university</p>
+                            <hr class="text-divider">
+                            <p class="presenter-infromation">he is founder of the IT department in altinbas</p>
+                        </div>
+                    </div>
+                    <div class="first-card">
+                        <div class="images">
+                            <img class="square-img" src="images/squers.jpg" alt="backgroundimg">
+                            <img class="presenter-img" src="images/sefer.jpg" alt="presenter-img">
+                        </div>
+                        <div class="card-text">
+                            <h2 class="presenter-name">Sefer Kurnaz</h2>
+                            <p class="presenter-descreption">Dr.sefer. faculaty of Engineering and Architecture</p>
+                            <hr class="text-divider">
+                            <p class="presenter-infromation">One of Altinbas Doctors Stuff </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="first-2-cards">
+                    <div class="first-card">
+                        <div class="images">
+                            <img class="square-img" src="images/squers.jpg" alt="backgroundimg">
+                            <img class="presenter-img" src="images/abdo.jpg" alt="presenter-img">
+                        </div>
+                        <div class="card-text">
+                            <h2 class="presenter-name">Abdullahi Ibrahim</h2>
+                            <p class="presenter-descreption">Dr.Abdullahi. faculaty of Engineering and Architecture</p>
+                            <hr class="text-divider">
+                            <p class="presenter-infromation">One of Altinbas Doctors Stuff</p>
+                        </div>
+                    </div>
+                    <div class="first-card">
+                        <div class="images">
+                            <img class="square-img" src="images/squers.jpg" alt="backgroundimg">
+                            <img class="presenter-img" src="images/mohammed.jpg" alt="presenter-img">
+                        </div>
+                        <div class="card-text">
+                            <h2 class="presenter-name">Mohammed Elias</h2>
+                            <p class="presenter-descreption">Dr.Mohammed. faculaty of Engineering and Architecture</p>
+                            <hr class="text-divider">
+                            <p class="presenter-infromation">One of Altinbas Doctors Stuff</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="first-2-cards">
+                    <div class="first-card">
+                        <div class="images">
+                            <img class="square-img" src="images/squers.jpg" alt="backgroundimg">
+                            <img class="presenter-img" src="images/timur1.png" alt="presenter-img">
+                        </div>
+                        <div class="card-text">
+                            <h2 class="presenter-name">Timur Inan</h2>
+                            <p class="presenter-descreption">Dr.Timur. faculaty of Engineering and Architecture</p>
+                            <hr class="text-divider">
+                            <p class="presenter-infromation">One of Altinbas Doctors Stuff</p>
+                        </div>
+                    </div>
+                    <div class="first-card">
+                        <div class="images">
+                            <img class="square-img" src="images/squers.jpg" alt="backgroundimg">
+                            <img class="presenter-img galip-img" src="images/galip1.jpg" alt="presenter-img">
+                        </div>
+                        <div class="card-text">
+                            <h2 class="presenter-name">Galip Cansever</h2>
+                            <p class="presenter-descreption">Dr.Timur. faculaty of Engineering and Architecture</p>
+                            <hr class="text-divider">
+                            <p class="presenter-infromation">One of Altinbas Doctors Stuff</p>
+                        </div>
+                    </div>
+                </div>`
+
+
+const cardContiner = document.querySelector(".all-feature-speakers-cards");
+
+
+window.addEventListener("load", () => {
+    cardContiner.innerHTML = featureCards
+})
+
